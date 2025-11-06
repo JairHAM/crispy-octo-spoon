2fb4b0a1517c233cf792562e605855ceb140cb7c
Jair HAM <2025021757@unfv.edu.pe>
2025-11-06 10:40:03 -0500

    trigger: Force redeploy with static files

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js - Updated for productoId fix
+// server.js - Updated for productoId fix + static files
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
